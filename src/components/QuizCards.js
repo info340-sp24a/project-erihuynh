40ced8af85ebeebc68dfefe5cbcedb745ca4b8e1
haoemilyy <haoemily@uw.edu>
2024-05-16 22:59:38 -0700

    fix console errors with search bar, add image alt text

--- src/components/QuizCards.js
+++ src/components/QuizCards.js
@@ -4,13 +4,13 @@ export function CardList(props) {
     const { searchValue } = props;
 
     const cardArray = [
-        { quizTitle: "who's your sanrio persona?", quizDes: "inspired by the sanrio characters", imgURL: "img/results.png" },
-        { quizTitle: "who's your ghibli persona?", quizDes: "inspired by Studio Ghibli films and characters", imgURL: "img/ponyo.jpg" },
-        { quizTitle: "what boba drink are you?", quizDes: "inspired by the boba cafes on the ave", imgURL: "img/boba.jpg" },
+        { quizTitle: "who's your sanrio persona?", quizDes: "inspired by the sanrio characters", imgURL: "img/results.png", imgAlt: "pompompurin" },
+        { quizTitle: "who's your ghibli persona?", quizDes: "inspired by Studio Ghibli films and characters", imgURL: "img/ponyo.jpg", imgAlt: "ponyo flying through sea" },
+        { quizTitle: "what boba drink are you?", quizDes: "inspired by the boba cafes on the ave", imgURL: "img/boba.jpg", imgAlt: "polar bear drinking boba" },
     ];
 
     const filteredCards = cardArray.filter(card => 
-        card.quizTitle.toLowerCase().includes(searchValue.toLowerCase())
+        card.quizTitle && typeof card.quizTitle === 'string' && card.quizTitle.toLowerCase().includes(searchValue.toLowerCase())
     );
 
     return (
@@ -22,6 +22,7 @@ export function CardList(props) {
                         quizTitle={card.quizTitle}
                         quizDes={card.quizDes}
                         imgURL={card.imgURL}
+                        imgAlt={card.imgAlt}
                     />
                 ))}
             </div>
@@ -30,7 +31,7 @@ export function CardList(props) {
 }
 
 export function QuizCard(props) {
-    const { quizTitle, imgURL, quizDes } = props;
+    const { quizTitle, imgURL, quizDes, imgAlt } = props;
 
     const handleClick = event => {
         console.log("Would navigate to create quiz page!");
@@ -40,7 +41,7 @@ export function QuizCard(props) {
         <div className="col-12 col-md-6 col-lg-4 p-2">
             <div className="card">
                 <div className="card-body">
-                    <img className="cardImgTop pb-3" src={imgURL} alt={quizTitle}/>
+                    <img className="cardImgTop pb-3" src={imgURL} alt={imgAlt}/>
                     <h3 className="card-title">{quizTitle}</h3>
                     <p className="card-text">{quizDes}</p>
                     <a className="btn customBtn" href="#" onClick={handleClick}>Take Quiz</a>
